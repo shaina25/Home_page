5f5fc8f57e575f8a476772c0f6fb2ef7afc4ba75
shaina25 <shrqmohd@gmail.com>
2020-04-01 22:23:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+function App (){
+  
+      return (
+        <body>
+        <div>
+        <ul>
+        
+        <button><li><a class="Home" id="Home">Log in</a></li></button>
+        <button> <li><a id="Freelancing" >Freelancing</a></li></button>
+        <button><li><a id="Community">Community</a></li></button>
+        <button><li><a id=" Publishing">Publishing</a></li></button>
+        <button><li><a id=" Opportunities">Opportunities</a></li></button>
+        <button><bi><a id="about">About</a></bi></button>
+
+        </ul>
+        <img src={process.env.PUBLIC_URL+"img2.PNG"} width="400" height="500"/>
+      <h1>HI , WE ARE NIRVANA</h1>
+      <h2>THE WORLD'S MOST LOVED SOCIAL</h2>
+      <h2>STORYTELLING PLATFORM. </h2>
+      <button2><b id="writing">START WRITING</b></button2>
+      <button2><b id="reading">START READING</b></button2>
+      
+    
+        </div>
+        </body>
+      
+
+      );
+   }
+
+
+
 
 export default App;
